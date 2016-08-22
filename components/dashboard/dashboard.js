3863f99cad766d2c7cc8e8409bb31abf8ada5423
awaisshah <awaisshah78@yahoo.com>
2016-08-22 14:09:31 +0500

    1) INSTALL GIT 2) for downloading dependencies type: npm install

---
+++ components/dashboard/dashboard.js
@@ -0,0 +1,214 @@
+/**
+ * Created by AwaisShah on 5/21/2016.
+ */
+angular.module("myApp")
+
+    .controller("DashboardController", ['$state','$scope','$log','$timeout', '$mdSidenav','$mdDialog'])
+
+//function DashboardController($state,$scope,$log,$timeout, $mdSidenav) {
+
+
+
+.controller('DashboardController', function ($scope, $timeout, $mdSidenav, $log,$mdDialog) {
+       $scope.user = firebase.auth().currentUser;
+
+    //-------------------------DIALOG BOX-----------------------------------------
+
+    $scope.showTabDialog = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+           // templateUrl: '../dashboard/tabDialog.tmpl.html',
+            templateUrl: '../../../SILAB web project/components/dashboard/tabDialog.tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true
+        })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+    };
+    $scope.showPrerenderedDialog = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            contentElement: '#myDialog',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+        });
+    };
+
+    //-------------------------END DIALOG BOX-----------------------------------------
+
+
+       // for user log in ?
+        firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log("Y USER : "+user);
+            // User is signed in.
+        } else {
+            // No user is signed in.
+        }
+        });
+
+    // UPDATE USER ----------------------------------------------------------------------------------------
+
+       /*
+        $scope.user.updateProfile({
+                 displayName: "SHAH G",
+                 uid: "Awais"
+             }).then(function() {
+                 // Update successful.
+             }, function(error) {
+        // An error happened.
+                });*/
+
+    // END UPDATE USER ----------------------------------------------------------------------------------------
+
+
+        console.log("USERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR>>>> : "+$scope.user);
+        //console.log("USERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR>>>> : "+$scope.user.uid);
+       // console.log("USERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR displayName>>>> : "+$scope.user.displayName);
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        $scope.isOpenRight = function(){
+            return $mdSidenav('right').isOpen();
+        };
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+            return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+            return debounce(function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }, 200);
+        }
+        function buildToggler(navID) {
+            return function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }
+        }
+    })
+        .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+            $scope.close = function () {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav('left').close()
+                    .then(function () {
+                        $log.debug("close LEFT is done");
+                    });
+            };
+        })
+        .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+            $scope.close = function () {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav('right').close()
+                    .then(function () {
+                        $log.debug("close RIGHT is done");
+                    });
+            };
+        })
+
+        function DialogController($scope, $mdDialog,$state) {
+            $state.go('dashboard.student');
+
+            $scope.student = {
+                name: '',
+                guardian: '',
+                guardian_type: '',
+                admissionDate: '',
+                parent_phone: '',
+                addmission_fees: 1000,
+                address: 'House # .., Street # .., .. Colony, Karachi.'
+            };
+            $scope.submit = function () {
+                $scope.student = {
+                    name:               $scope.student.name,
+                    guardian:           $scope.student.guardian,
+                    guardian_type:      $scope.student.guardian_type,
+                    admissionDate:      $scope.student.admissionDate,
+                    parent_phone:       $scope.student.parent_phone,
+                    addmission_fees:    $scope.student.addmission_fees,
+                    address:            $scope.student.address
+                };
+                console.log("object s >>>>>>>",$scope.student);
+            };
+
+            $scope.adder = function () {
+                alert("pakistan");
+                console.log("ADDDDDDDDDDDDDDDDDDDDDDDDDDDDDERRRRRRRRRRRRRRRRRRRRRRRRRRR FUNC()")
+                $state.go('dashboard.student')
+                //$state.go('dashboard.student1');
+            }
+            $scope.hide = function() {
+                $mdDialog.hide();
+            };
+            $scope.cancel = function() {
+                $mdDialog.cancel();
+            };
+            $scope.answer = function(answer) {
+                $mdDialog.hide(answer);
+            };
+        };
+
+
+
+
+    /*var tabs = [
+            { title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
+            { title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
+            { title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
+            { title: 'Four', content: "If you set the selected tab binding to -1, it will leave no tab selected."},
+            { title: 'Five', content: "If you remove a tab, it will try to select a new one."},
+            { title: 'Six', content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
+            { title: 'Seven', content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."},
+            { title: 'Eight', content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"},
+            { title: 'Nine', content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."},
+            { title: 'Ten', content: "If you're still reading this, you should just go check out the API docs for tabs!"}
+        ],
+        selected = null,
+        previous = null;
+    $scope.tabs = tabs;
+    $scope.selectedIndex = 2;
+    $scope.$watch('selectedIndex', function(current, old){
+        previous = selected;
+        selected = tabs[current];
+        if ( old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
+        if ( current + 1 )                $log.debug('Hello ' + selected.title + '!');
+    });
+    $scope.addTab = function (title, view) {
+        view = view || title + " Content View";
+        tabs.push({ title: title, content: view, disabled: false});
+    };
+    $scope.removeTab = function (tab) {
+        var index = tabs.indexOf(tab);
+        tabs.splice(index, 1);
+    };*/
+    
+//}
---
+++ core/config.js
@@ -0,0 +1,129 @@
+/**
+ * Created by AwaisShah on 5/21/2016.
+ */
+(function () {
+    angular.module("myApp")
+        .config(function ($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+
+
+                .state("dashboard", {
+                    url: '/dashboard',
+                    templateUrl: 'components/dashboard/dashboard.html',
+                    controller: 'DashboardController',
+                    controllerAs: 'dashboard'
+
+                })
+                .state('dashboard.student', {
+                    url: '/student',
+                    //loginCompulsory: true,
+                    views: {
+                        'show': {
+                            templateUrl: "../../SILAB web project/components/dashboard/student/student.html",
+                            controllerAs: "student",
+                            controller: "StudentController"
+                        }
+                    }
+                })
+                .state('dashboard.student.add', {
+                    url: '/add',
+                    //loginCompulsory: true,
+                    views: {
+                        'menuContent': {
+                            templateUrl: "../components/student/add/add.html",
+                            controllerAs: "add",
+                            controller: "AddController"
+                        }
+                    }
+                })
+
+                .state("login", {
+                    url: "/login",
+                    templateUrl: "components/login/login.html",
+                    controller: "LoginController",
+                    controllerAs: "login"
+                })
+                .state("panel", {
+                    url: "/panel/:id",
+                    templateUrl: "components/panel/panel.html",
+                    controller: "PanelController",
+                    controllerAs: "panel"
+                })
+               
+                .state("principal", {
+                    url: "/principal",
+                    templateUrl: "components/principal/principal.html",
+                    controller: "PrincipalController",
+                    controllerAs: "principal"
+                })
+              /*  .state("student", {
+                    url: "/student1",
+                    templateUrl: "components/student1/student1.html",
+                    controller: "StudentController",
+                    controllerAs: "student"
+                })*/
+
+
+               /* .state("student1.add", {
+                    url: "/add",
+                    views: {
+                        'menuContent': {
+                            templateUrl: "components/student1/add/add.html",
+                            controller: "AddController",
+                            controllerAs: "add"
+                        }
+                    }
+                })*/
+
+                .state('student.add', {
+                    url: '/add',
+                    //loginCompulsory: true,
+                    views: {
+                        'menuContent': {
+                            templateUrl: "components/student1/add/add.html",
+                            controllerAs: "add",
+                            controller: "AddController"
+                        }
+                    }
+                })
+                /*.state("panel", {
+                    url: "/panel",
+                    templateUrl: "../components/panel/panel.html",
+                    controller: "PanelController",
+                    controllerAs: "panel"
+                })*/
+                
+                .state("teacher", {
+                    url: "/teacher",
+                    templateUrl: "components/teacher/teacher.html",
+                    controller: "TeacherController",
+                    controllerAs: "teacher"
+            })
+            .state('teacher.add', {
+                url: '/add',
+                //loginCompulsory: true,
+                views: {
+                    'abc': {
+                        templateUrl: "components/teacher/add/add.html",
+                        controllerAs: "add",
+                        controller: "AddTeacherController"
+                    }
+                }
+            });
+            
+              /*  .state("edit", {
+                    url: "/edit/:id",
+                    templateUrl: "components/edit/edit.html",
+                    controller: "EditController",
+                    controllerAs: "edit"
+
+
+                });
+*/
+
+            $urlRouterProvider.otherwise('/login')
+
+        })
+
+})();
